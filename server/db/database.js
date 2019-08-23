ee620c447d511589b6e35157072444d57e575ada
Andy <github@sockbot.com>
2019-08-22 19:58:54 -0700

    refactor to node-postgres suggested project structure

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
 const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USER,
