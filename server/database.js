942ff8276e7659f833256123deb500ebd1720c52
Andy <github@sockbot.com>
2019-08-22 19:42:00 -0700

    implement add listing to push to db

--- server/database.js
+++ server/database.js
@@ -190,9 +190,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [];
+  for (const param in property) {
+    queryParams.push(property[param]);
+  }
+
+  return pool.query(`
+  INSERT INTO properties
+  (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+  VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+  )
+  RETURNING *;
+  `, queryParams)
+  .then(res => {
+    console.log(res.rows[0])
+    return res.rows[0];
+  })
 }
 exports.addProperty = addProperty;
